105cdb7e029973e4f45617d54867f3f4e3636df5
RamS24 <AlSaadi.Rami24@gmail.com>
2022-11-07 23:55:36 +0100

    sol

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./style.css";
 import Client from "./utils/Client.js";
 import View from "./View";
@@ -8,37 +8,77 @@ export default function App() {
   const [search, setSearch] = useState('');
   const [movieList, setMovieList] = useState([]);
 
+
+  useEffect( ()=>
+  {
+    if(localStorage?.getItem('list') && localStorage?.getItem('list')[0])
+    getMovie((JSON.parse(localStorage?.getItem('list'))));
+  }
+,[])
+
+
+  function updateMovieList(data){
+    setMovieList(prev=>[data, ...prev]);
+  }
+
+  function getMovie(search){
+    if(!(search instanceof Array && search[0]))
+    try {
+      console.log('clicked Enter')
+        client.getMovieData(search.toLowerCase()).then((data) => {
+            if (data && !(data.Response === "False")) {
+                if (
+                  movieList.findIndex(movie=> movie.Title==data.Title)<0
+                ) {
+                    updateMovieList(data);
+                } else alert("You have already added this movie.");
+            } else
+                alert(
+                    `${JSON.stringify(data.Error)}\nSorry, ${search} movie was not found!`
+                );
+            setSearch("");
+        });
+    } catch (error) {
+        alert(`Error adding movie: ${error}`);
+    }
+    else if(search instanceof Array && search[0])
+    {const movies=[];
+    for(const item of search)
+    try {
+      console.log('clicked Enter')
+        client.getMovieData(item.toLowerCase()).then((data) => {
+            if (data && !(data.Response === "False")) {
+                if (
+                  movieList.findIndex(movie=> movie.Title==data.Title)<0
+                ) {
+                    movies.push(data);
+                } else console.log("You have already added this movie.");
+            } else
+                alert(
+                    `${JSON.stringify(data.Error)}\nSorry, ${item} movie was not found!`
+                );
+                if(search.length-1 === search.lastIndexOf(item))
+            setMovieList([...movies]);
+        });
+    } catch (error) {
+        alert(`Error adding movie: ${error}`);
+    }
+  }
+  return true
+  }
+
+
 function onEnter(e){
     // add movie
-      console.log(search);
       if (search)
-      try {
-              console.log('clicked Enter')
-                client.getMovieData(search.toLowerCase()).then((data) => {
-                  console.log(movieList, data.Title);
-                    if (data && !(data.Response === "False")) {
-                        if (
-                          movieList.findIndex(movie=> movie.Title==data.Title)<0
-                        ) {
-                            setMovieList(prev=>[data, ...prev]);
-                        } else alert("You have already added this movie.");
-                    } else
-                        alert(
-                            `${JSON.stringify(data.Error)}\nSorry, ${search} movie was not found!`
-                        );
-                    setSearch("");
-                });
-            } catch (error) {
-                alert(`Error adding movie: ${error}`);
-            }
+      getMovie(search);
     }
 
 
   return (
     <div>
-      <h1>Movie App!</h1>
       <main className="container">
-        <h2 className="title">My Movies</h2>
+        <h1 className="title">Movie Search App</h1>
         <input
           type="text"
           className="movie-input"
@@ -49,7 +89,14 @@ function onEnter(e){
           onKeyDown={e=>{e.key === "Enter"? onEnter(e):''}}
         />
         <section className="buttons">
-          <button className="btn-reset" onClick={()=>setMovieList([])}>Reset</button>
+          <button className="btn-reset" onClick={()=>{setMovieList([]); localStorage.removeItem('list')}}>Reset</button>
+          <button className="btn-save" onClick={()=>
+          {
+            const titles = [];
+            movieList.forEach(movie=>titles.push(movie.Title));
+            localStorage.setItem('list', JSON.stringify(titles.reverse()))
+            }
+          }>Save List</button>
         </section>
         <section className="movies">
         {
