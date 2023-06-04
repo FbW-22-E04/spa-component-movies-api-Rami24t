19fe0e9098b397d87f3da44a086c0fadcbd12dcb
RamS24 <AlSaadi.Rami24@gmail.com>
2023-06-04 20:31:14 +0200

    add add button

--- src/App.js
+++ src/App.js
@@ -5,75 +5,66 @@ import View from "./View";
 
 export default function App() {
   const client = new Client();
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [movieList, setMovieList] = useState([]);
 
-
-  useEffect( ()=>
-  {
-    const ls = localStorage.getItem('list');
-    if(!ls)
-      return
+  useEffect(() => {
+    const ls = localStorage.getItem("list");
+    if (!ls) return;
     const lsJson = JSON.parse(ls);
-    if(lsJson && lsJson.length)
-    getMovie((JSON.parse(ls)));
-  }
-,[])
+    if (lsJson && lsJson.length) getMovie(JSON.parse(ls));
+  }, []);
 
-
-  function updateMovieList(data){
-    setMovieList(prev=>[data, ...prev]);
+  function updateMovieList(data) {
+    setMovieList((prev) => [data, ...prev]);
   }
 
-  function getMovie(search){
-    if(!(search instanceof Array && search[0]))
-    try {
-      console.log('clicked Enter')
+  function getMovie(search) {
+    if (!(search instanceof Array && search[0]))
+      try {
+        console.log("clicked Enter");
         client.getMovieData(search.toLowerCase()).then((data) => {
-            if (data && !(data.Response === "False")) {
-                if (
-                  movieList.findIndex(movie=> movie.Title==data.Title)<0
-                ) {
-                    updateMovieList(data);
-                } else alert("You have already added this movie.");
-            } else
-                alert(
-                    `${JSON.stringify(data.Error)}\nSorry, ${search} movie was not found!`
-                );
-            setSearch("");
+          if (data && !(data.Response === "False")) {
+            if (movieList.findIndex((movie) => movie.Title == data.Title) < 0) {
+              updateMovieList(data);
+            } else alert("You have already added this movie.");
+          } else
+            alert(
+              `${JSON.stringify(
+                data.Error
+              )}\nSorry, ${search} movie was not found!`
+            );
+          setSearch("");
         });
-    } catch (error) {
+      } catch (error) {
         alert(`Error adding movie: ${error}`);
-    }
-    else if(search instanceof Array && search[0])
-    {const movies=[];
-    for(const item of search)
-    try {
-      console.log('loading movie ' + item +' from API')
-        const promise = client.getMovieData(item.toLowerCase());
-                    movies.push(promise);
-                if(search.length-1 === search.lastIndexOf(item))
-                {
-                  Promise.all(movies).then(values=>{ console.log(values); 
-                    values = values.filter(value=>value.Title);
-                    setMovieList(values)});
-                  console.log(search.length-1);
-                }
+      }
+    else if (search instanceof Array && search[0]) {
+      const movies = [];
+      for (const item of search)
+        try {
+          console.log("loading movie " + item + " from API");
+          const promise = client.getMovieData(item.toLowerCase());
+          movies.push(promise);
+          if (search.length - 1 === search.lastIndexOf(item)) {
+            Promise.all(movies).then((values) => {
+              console.log(values);
+              values = values.filter((value) => value.Title);
+              setMovieList(values);
+            });
+            console.log(search.length - 1);
+          }
+        } catch (error) {
+          alert(`Error adding movie: ${error}`);
         }
-      catch (error) {
-        alert(`Error adding movie: ${error}`);
     }
-  }
-  return true
+    return true;
   }
 
-
-function onEnter(e){
+  function onEnter(e) {
     // add movie
-      if (search)
-      getMovie(search);
-    }
-
+    if (search) getMovie(search);
+  }
 
   return (
     <div>
@@ -85,23 +76,40 @@ function onEnter(e){
           id="input"
           placeholder="Add a movie"
           value={search}
-          onChange={(e)=>setSearch(e.target.value)}
-          onKeyDown={e=>{e.key === "Enter"? onEnter(e):''}}
+          onChange={(e) => setSearch(e.target.value)}
+          // onBlur={onEnter}
+          onKeyDown={(e) => {
+            e.key === "Enter" ? onEnter(e) : "";
+          }}
         />
         <section className="buttons">
-          <button className="btn-reset" onClick={()=>{setMovieList([]); localStorage.removeItem('list')}}>Reset</button>
-          <button className="btn-save" onClick={()=>
-          {
-            const titles = [];
-            movieList.forEach(movie=>titles.push(movie.Title));
-            localStorage.setItem('list', JSON.stringify(titles.reverse()))
-            }
-          }>Save List</button>
+          <button className="btn-add" onClick={onEnter}>
+            Add
+          </button>
+          <button
+            className="btn-save"
+            onClick={() => {
+              const titles = [];
+              movieList.forEach((movie) => titles.push(movie.Title));
+              localStorage.setItem("list", JSON.stringify(titles.reverse()));
+            }}
+          >
+            Save List
+          </button>
+          <button
+            className="btn-reset"
+            onClick={() => {
+              setMovieList([]);
+              localStorage.removeItem("list");
+            }}
+          >
+            Reset
+          </button>
         </section>
         <section className="movies">
-        {
-        movieList?.map((data,idx)=> <View key={idx} data={data} />)
-      }
+          {movieList?.map((data, idx) => (
+            <View key={idx} data={data} />
+          ))}
         </section>
       </main>
     </div>
